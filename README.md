5100cb052c6c3283a92ccc07da5dd7818da9a098
Elaria <elarya.latif@gmail.com>
2018-02-23 01:21:31 +0200

    main files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fahad-Al-Kubaisi-
---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head dir="rtl" lang="ar">
+    <!--  Links -->
+    <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <!-- Title -->
+    <title>Gamzly</title>
+</head>
+<body>
+    <section class="">
+        <div class="content">
+            <!-- Logo -->
+            <div class="logo">
+                <img src="./assets/images/Logo.png" alt="">
+                <h1>
+                  <span>إنتظرونا قريبا</span>
+                </h1>
+            </div>
+            <!-- End of logo -->
+            <!-- Countdown -->
+            <div class="countdown">
+                <h2>
+                    <label class="num" for="">29&nbsp;:&nbsp;</label>
+                    <label class="date" for="">يوم</label>
+                </h2>
+
+                <h2>
+                    <label class="num" for="">23&nbsp;:&nbsp;</label>
+                    <label class="date" for="">ساعة</label>
+                </h2>
+                <h2>
+                    <label class="num" for="">55&nbsp;:&nbsp;</label>
+                    <label class="date" for="">دقيقة</label>
+                </h2>
+                <h2>
+                    <label class="num" for="">24</label>
+                    <label class="date" for="">ثانية</label>
+                </h2>
+            </div>
+            <!-- End of Countdown -->
+            <!-- registration form -->
+            <div class="registration">
+                <p>*أدخل الأن بريدك الإلكتروني و ستحصل على خصم 10% على اي لعبة؛ لأول 200 مسجل داخل مملكة السعودية !
+                </p>
+                <form action="./sendmail.php">
+                  <label>
+                    <input type="email" placeholder="البريد الإلكتروني">
+                  </label>
+                  <button>سجل الآن</button>
+                </form>
+            </div>
+            <!-- End ofregistration form -->
+            <!-- soial media -->
+            <div class="social_media">
+                <a href=""><img src="./assets/images/fb.png" alt=""></a>
+                <a href=""><img src="./assets/images/twitter.png" alt=""></a>
+                <a href=""><img src="./assets/images/insta.png" alt=""></a>
+                <a href=""><img src="./assets/images/Snap.png" alt=""></a>
+            </div>
+            <!-- End of social media -->
+        </div>
+    </section>
+    <!-- Scripts  -->
+    <script src="./assets/js/jquery-3.1.1.min.js" type="text/javascript"></script>
+    <script src="./assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="https://use.fontawesome.com/4360cae271.js"></script>
+    <script src="./assets/js/ui.js" type="text/javascript"></script>
+</body>
+
+</html>
